b2ab1e54cb5a2f75513483e7f1029bf870e7eb13
ahadsenja <hamdanilm96@gmail.com>
2019-10-19 04:37:27 +0800

    adding navbar

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+
+class NavigationBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navbar bg='light'>
+                    <Navbar.Brand>SPK-SAW</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='mr-auto'>
+                            <Nav.Link>About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
+export default NavigationBar
