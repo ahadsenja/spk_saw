5299177bace8c5a4e6d7709926f42f0e887e1295
ahadsenja <hamdanilm96@gmail.com>
2019-11-01 21:44:10 +0800

    update app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import Assessment from './app/Assessment/Assessment';
+import { Row, Col } from 'react-bootstrap'
+import Assessment from './app/Assessment/Assessment'
+import NavigationBar from './components/NavigationBar/NavigationBar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <Assessment />
+    <div className="App" style={{ overflow: 'hidden' }}>
+      <NavigationBar />
+      <Row>
+        <Col sm='2'> <Sidebar /> </Col>
+        <Col sm='10'>
+          <div className='container-fluid'>
+            <main style={{ display: 'overview', paddingTop: '20px' }}>
+              <Assessment />
+            </main>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
